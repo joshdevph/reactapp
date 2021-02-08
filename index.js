4a1fd894e1f3ee88a925c861a9f37874ac7b1195
Joshua Alexes Dignadice <joshuadignadice24@gmail.com>
2021-02-08 17:08:35 +0800

    Edit index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ mongoose.connect(process.env.MONGGO_URI,
 if(process.env.NODE_ENV === 'production'){
     
     app.use(express.static(__dirname + '/client/build'))
-    app.get('/*', function(req, res) {
+    app.get('*', function(req, res) {
         res.sendFile(path.join(__dirname, '/client/build/index.html'), function(err) {
           if (err) {
             res.status(500).send(err)
